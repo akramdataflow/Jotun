46c91bf414a70870852d3657d00326cc4d34745b
akramdataflow <akramdataflowp@gmail.com>
2024-12-25 20:16:41 +0300

    v3

--- jotun/jotun/settings.py
+++ jotun/jotun/settings.py
@@ -55,15 +55,15 @@ ROOT_URLCONF = "jotun.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': ['templates'],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # Ensure this includes the correct template directories
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
--- jotun/main/models.py
+++ jotun/main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from colorfield.fields import ColorField
+
 
 # Create your models here.
 
@@ -19,6 +21,7 @@ class Products(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)  
+    color = ColorField(default='#FFFFFF')
 
     def __str__(self):
         return self.name
